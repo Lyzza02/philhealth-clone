7a7cd1e6c03dc737702e91e672c377533cbe423a
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2023-11-02 07:26:34 +0800

    [MOD] sample footerSection

--- philhealth-react/src/components/FooterSection.tsx
+++ philhealth-react/src/components/FooterSection.tsx
@@ -14,20 +14,35 @@ function FooterSection() {
           <Footer>
             <Container>
               <Content style={{ textAlign: 'center' }}>
+
+                <img
+                  style={{ width: '140px', height: 'auto' }}
+                  alt="Philhealth logo"
+                  src="https://www.philhealth.gov.ph/images/ph_logo0.png"
+                />
+
+                <div>
+                  <img
+                    style={{ width: '60px', height: '60px' }}
+                    alt=" logo1"
+                    src="https://www.philhealth.gov.ph/images/transparency_seal.png"
+                  />
+                  <img
+                    style={{ width: '60px', height: '60px' }}
+                    alt="logo2"
+                    src="https://www.philhealth.gov.ph/images/corp_gov.png"
+                  />
+                  <img
+                    style={{ width: '60px', height: '60px' }}
+                    alt="logo3"
+                    src="https://www.philhealth.gov.ph/images/foi_logo.png"
+                  />
+                </div>
+
                 <p>
-                  <strong>Bulma</strong> by{' '}
-                  <a href="http://jgthms.com">Jeremy Thomas</a>. The source code
-                  is licensed under
-                  <a href="http://opensource.org/licenses/mit-license.php">
-                    {' '}
-                    MIT
-                  </a>
-                  . The website content is licensed{' '}
-                  <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-                    CC BY NC SA 4.0
-                  </a>
-                  .
+                  © 2014 Philippine Health Insurance Corporation | Citystate Centre, 709 Shaw Boulevard 1603 Pasig City | Action Center 8441-7442
                 </p>
+
               </Content>
             </Container>
           </Footer>
